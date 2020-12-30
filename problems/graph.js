5b5c8eb9d137186ec31dfcad523b71e89a9bbf87
Gabriel Harris <gch910@gmail.com>
2020-12-30 12:29:13 -0600

    addVertex and addEdges

--- problems/graph.js
+++ problems/graph.js
@@ -2,14 +2,33 @@
 class Graph {
   constructor() {
     // Code goes here ...
+    this.adjList = {};
   }
 
   addVertex(vertex) {
     // Code goes here ...
+    if (!(vertex in this.adjList)) this.adjList[vertex] = [];
+
   }
 
   addEdges(srcValue, destValue) {
     // Code goes here ...
+    //check if they exist in the graph
+    this.addVertex(srcValue);
+    this.addVertex(destValue);
+    let value1 = this.adjList[srcValue];
+    let value2 = this.adjList[destValue];
+
+    value1.push(destValue);
+    value2.push(srcValue);
+    //value2.push(value1);
+
+    
+  
+    //if not, add them as values
+    //push values into corresponding neighbors array;
+
+
   }
 
   buildGraph(edges) {
@@ -29,6 +48,8 @@ class Graph {
   }
 
 }
+
+
 
 module.exports = {
   Graph
