41b620888fefde544902014d218125f75ad71f1f
Gabriel Harris <gch910@gmail.com>
2020-12-30 16:10:06 -0600

    breadthfirst and depth first iterative

--- problems/graph.js
+++ problems/graph.js
@@ -1,4 +1,3 @@
-
 class Graph {
   constructor() {
     // Code goes here ...
@@ -8,7 +7,6 @@ class Graph {
   addVertex(vertex) {
     // Code goes here ...
     if (!(vertex in this.adjList)) this.adjList[vertex] = [];
-
   }
 
   addEdges(srcValue, destValue) {
@@ -23,39 +21,96 @@ class Graph {
     value2.push(srcValue);
     //value2.push(value1);
 
-    
-  
     //if not, add them as values
     //push values into corresponding neighbors array;
-
-
   }
 
   buildGraph(edges) {
     for (let i = 0; i < edges.length; i++) {
       const el = edges[i];
-     this.addVertex(el[0])
-     this.addVertex(el[1])
-     this.addEdges(el[0],el[1])
-     
+      this.addVertex(el[0]);
+      this.addVertex(el[1]);
+      this.addEdges(el[0], el[1]);
     }
-    return this.adjList
+    return this.adjList;
   }
 
   breadthFirstTraversal(startingVertex) {
-    // Code goes here ...
+    const visited = [];
+    const queue = [startingVertex];
+
+    while (queue.length) {
+      let el1 = queue.shift();
+      let el2 = this.adjList[el1];
+
+      el2.forEach((val) => {
+        if (!visited.includes(val)) {
+          queue.push(val);
+        }
+      });
+
+      if (!visited.includes(el1)) visited.push(el1);
+    }
+    return visited;
   }
 
   depthFirstTraversalIterative(startingVertex) {
-    // Code goes here ...
+    let visited = [];
+    //let visited = new Set()
+    let stack = [startingVertex];
+  
+
+    //while loop
+    while (stack.length) {
+      let current = stack.pop();
+      let neighbors = this.adjList[current];
+
+      if (!visited.includes(current)) {
+        //if (!visited.has(current))
+        visited.push(current);
+        //visited.add(current)
+        for (let i = 0; i < neighbors.length; i++) {
+          let neighbor = neighbors[i];
+          stack.push(neighbor);
+        }
+      }
+
+   
+    }
+
+    //let array = Array.from(visited)
+    //return array;
+    return visited;
   }
 
-  depthFirstTraversalRecursive(startingVertex, visited = new Set(), vertices = []) {
+  //store values in visited and return at end
+
+  //   a: ['b', 'c', 'd'],
+  //  visited = ['a', 'd']
+  //  stack = ['b', 'c', 'g']
+
+  depthFirstTraversalRecursive(
+    startingVertex,
+    visited = new Set(),
+    vertices = []
+  ) {
     // Code goes here ...
   }
-
 }
 
+// const graph = new Graph()
+// graph.adjList = {
+//   a: ['b', 'c', 'd'],
+//   b: ['a', 'c', 'e'],
+//   c: ['a', 'b', 'f', 'g'],
+//   d: ['a', 'g'],
+//   g: ['d', 'c', 'f'],
+//   e: ['b'],
+//   f: ['c', 'g']
+// }
+
+// graph.breadthFirstTraversal('a')
+
 module.exports = {
-  Graph
+  Graph,
 };
